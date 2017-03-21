10afe877e9f39037da51fd8e26ce3d889339f982
Johannes Ridderstedt <johannes@46elks.com>
2017-03-21 14:57:44 +0100

    Example 4 game result rendering.

--- Hello4.elm
+++ Hello4.elm
@@ -2,13 +2,14 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
+primes = [1,2,3,5,7,11,13,17,19,23]
 model = []
 main = beginnerProgram { model = model, view = view, update = update }
 
 update msg model =
   let
     nextNumber = (List.length model) + 1
-    isPrime = List.member nextNumber [1,2,3,5,7,11,13,17,19,23]
+    isPrime = List.member nextNumber primes
     correctAnswer = msg == isPrime
   in
     case correctAnswer of
@@ -35,6 +36,22 @@ choice msg title =
 
 view model =
   div []
-    [ choice True "Is prime! :-)"
+    [ choice True "Prime! :-)"
     , choice False "Not a prime!"
+    , render model
     ]
+
+render model =
+  let
+    nextNumber = List.length model + 1
+    answerView happy =
+      case happy of
+        True ->
+          img [ style [("height","64px")], src "happy-cc0.jpg" ] []
+        False ->
+          img [ style [("height","16px")], src "happy-cc0.jpg" ] []
+  in
+    div []
+      [ div [] (List.map answerView model)
+      , h1 [] [ text ("Is " ++ (toString nextNumber) ++ " a prime?") ]
+      ]
